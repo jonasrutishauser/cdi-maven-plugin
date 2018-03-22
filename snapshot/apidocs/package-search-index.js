e22a160b3f024734ee7e42c63f070fee1a00cbf9
Travis CI User <travis@example.org>
2018-03-22 21:57:36 +0000

    [wagon-git] adding files to repository

---
+++ snapshot/apidocs/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"com.github.jonasrutishauser.cdi.maven.plugin"},{"l":"com.github.jonasrutishauser.cdi.maven.plugin.ear"},{"l":"com.github.jonasrutishauser.cdi.maven.plugin.war"},{"l":"com.github.jonasrutishauser.cdi.maven.plugin.weld"},{"l":"com.github.jonasrutishauser.cdi.maven.plugin.weld.bootstrap"},{"l":"com.github.jonasrutishauser.cdi.maven.plugin.weld.services"}]
